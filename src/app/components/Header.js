e7cb2161b1b1bebb3828f5b90a79caf49edd8221
TheoB82 <grandpatassos@gmail.com>
2025-03-16 21:02:07 +0000

    header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -85,32 +85,32 @@ const Header = () => {
         <ul className="flex space-x-6 text-lg font-semibold tracking-tight">
           {/* Recipes Dropdown */}
           <li
-            className="relative"
-            ref={dropdownRef}
-            onMouseEnter={() => setIsDropdownOpen(true)}
-            onMouseLeave={() => setIsDropdownOpen(false)}
-          >
-            <span className="text-gray-700 hover:text-blue-500 transition-colors duration-300 cursor-pointer">
-              {language === "EN" ? "Recipes" : "Συνταγές"}
-            </span>
-            {isDropdownOpen && (
-              <ul className="absolute left-0 mt-2 w-48 bg-white border border-gray-300 shadow-lg rounded-md z-50">
-                {categoryMapping[language] && categoryMapping[language].length > 0 ? (
-                  categoryMapping[language].map((category) => (
-                    <li
-                      key={category.path}
-                      className="p-2 hover:bg-gray-100 cursor-pointer"
-                      onClick={() => handleCategoryClick(category.path)}
-                    >
-                      {category.name}
-                    </li>
-                  ))
-                ) : (
-                  <li className="p-2 text-gray-500">No categories available</li>
-                )}
-              </ul>
-            )}
-          </li>
+  className="relative"
+  ref={dropdownRef}
+  onMouseEnter={() => setIsDropdownOpen(true)}
+  onMouseLeave={() => setIsDropdownOpen(false)}
+>
+  <span className="text-gray-700 hover:text-blue-500 transition-colors duration-300 cursor-pointer">
+    {language === "EN" ? "Recipes" : "Συνταγές"}
+  </span>
+  <div
+    className={`absolute left-0 mt-2 w-48 bg-white border border-gray-300 shadow-lg rounded-md z-50 ${isDropdownOpen ? 'block' : 'hidden'}`}
+  >
+    {categoryMapping[language] && categoryMapping[language].length > 0 ? (
+      categoryMapping[language].map((category) => (
+        <li
+          key={category.path}
+          className="p-2 hover:bg-gray-100 cursor-pointer"
+          onClick={() => handleCategoryClick(category.path)}
+        >
+          {category.name}
+        </li>
+      ))
+    ) : (
+      <li className="p-2 text-gray-500">No categories available</li>
+    )}
+  </div>
+</li>
           <li>
             <Link href="/about" className="text-gray-700 hover:text-blue-500">
               {language === "EN" ? "About Grandpa" : "Σχετικά με τον Παππού"}
