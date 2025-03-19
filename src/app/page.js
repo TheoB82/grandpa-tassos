6a27d7b77f54bbeafad10cfeae0cb127311d5a10
TheoB82 <grandpatassos@gmail.com>
2025-03-19 20:43:51 +0000

    Fix import path typo for categoryMapping

--- src/app/page.js
+++ src/app/page.js
@@ -36,17 +36,18 @@ export default function Home() {
       </h1>
 
       {/* Embedded YouTube Video */}
-      <div className="relative pb-[56.25%] h-0 mb-10 w-full max-w-4xl mx-auto">
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '40px' }}>
   <iframe
-    className="absolute top-0 left-0 w-full h-full rounded-xl shadow-lg"
+    width="560"
+    height="315"
     src="https://www.youtube.com/embed/domQLeVFwfQ"
     title="YouTube video player"
     frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerPolicy="strict-origin-when-cross-origin"
     allowFullScreen
   ></iframe>
 </div>
+
 
       {/* Recipe Grid */}
       <RecipeGrid recipes={filteredRecipes} language={language} />
