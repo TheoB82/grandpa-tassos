48b47704fb8c7b2a67996d4f066fcd91b130cfa0
TheoB82 <grandpatassos@gmail.com>
2025-03-19 22:27:13 +0000

    Updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Link to Google Fonts for Roboto (if still needed) */}
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
-          
           {/* Add local font (Mynerve) */}
           <style>{`
             @font-face {
