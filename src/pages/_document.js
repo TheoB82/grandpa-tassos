c023df9db8c5ca14be437c97d4bcfc56f42e40d1
TheoB82 <grandpatassos@gmail.com>
2025-03-19 22:20:16 +0000

    Updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,21 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          {/* Add custom fonts here */}
+          {/* Link to Google Fonts for Roboto (if still needed) */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" />
+          
+          {/* Add local font (Mynerve) */}
+          <style>{`
+            @font-face {
+              font-family: 'Mynerve';
+              src: url('/fonts/Mynerve.Regular.ttf') format('truetype');
+              font-weight: normal;
+              font-style: normal;
+              font-display: swap;
+            }
+          `}</style>
         </Head>
         <body>
           <Main />
