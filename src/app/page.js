69ccc920f529d9b23da996893cf31ceadf57acad
TheoB82 <grandpatassos@gmail.com>
2025-03-30 22:04:03 +0100

    Updates

--- src/app/page.js
+++ src/app/page.js
@@ -37,15 +37,7 @@ export default function Home() {
 
       {/* Embedded YouTube Video */}
       <div className="flex justify-center mb-10 min-h-[315px]">
-  <iframe
-    className="rounded-xl shadow-lg w-[560px] h-[315px]"
-    src="https://www.youtube.com/watch?v=f_ZUEhudAWI"
-    title="YouTube video player"
-    frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    referrerPolicy="strict-origin-when-cross-origin"
-    allowFullScreen
-  ></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/f_ZUEhudAWI?si=_-4Zyo3ZNiyjGJzt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
 
 
