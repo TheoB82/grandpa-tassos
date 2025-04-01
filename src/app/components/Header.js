a37cb82612e6df4ade9fa9f07a22f67c87ed5157
TheoB82 <grandpatassos@gmail.com>
2025-04-01 22:35:20 +0100

    Updates2

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -131,45 +131,44 @@ const Header = () => {
       onChange={(e) => setSearchQuery(e.target.value)}
       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
-     {searchResults.length > 0 && (
-              <ul className="absolute left-0 mt-2 w-72 bg-white border border-gray-300 shadow-lg rounded-md z-50 max-h-64 overflow-y-auto">
-                {searchResults.map((recipe) => (
-                 <li
-                 key={recipe.TitleEN}
-                 className="p-2 hover:bg-gray-100 cursor-pointer"
-                 onClick={(event) => handleRecipeClick(recipe, event)}
-               >
-                 <div className="font-semibold">{recipe[`Title${language}`]}</div>
-                 <div className="text-sm text-gray-600">{recipe[`ShortDescription${language}`]}</div>
-               </li>
-               
-                ))}
-              </ul>
-            )}
-          </div>
+    {searchResults.length > 0 && (
+      <ul className="absolute left-0 mt-2 w-full bg-white border border-gray-300 shadow-lg rounded-md z-50 max-h-64 overflow-y-auto">
+        {searchResults.map((recipe) => (
+          <li
+            key={recipe.TitleEN}
+            className="p-2 hover:bg-gray-100 cursor-pointer"
+            onClick={(event) => handleRecipeClick(recipe, event)}
+          >
+            <div className="font-semibold">{recipe[`Title${language}`]}</div>
+            <div className="text-sm text-gray-600">{recipe[`ShortDescription${language}`]}</div>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
 )}
 
-      {/* Mobile Dropdown Menu */}
-      {isMenuOpen && (
-        <div className="lg:hidden mt-4 bg-white border-t border-gray-200 pt-4">
-          <div className="relative">
-            <button
-              className="block px-4 py-2 text-gray-700 hover:bg-gray-100 w-full text-left"
-              onClick={() => setIsMobileDropdownOpen(!isMobileDropdownOpen)}
-            >
-              {language === "EN" ? "Recipes" : "Συνταγές"}
-            </button>
-            {isMobileDropdownOpen && (
-              <ul className="space-y-2 pl-4">
-                {categoryMapping[language]?.map((category) => (
-                  <li key={category.path} className="p-2 hover:bg-gray-100">
-                    <Link href={category.path} onClick={() => handleCategoryClick(category.path)}>
-                      {category.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
+{/* Mobile Dropdown Menu */}
+{isMenuOpen && (
+  <div className="lg:hidden mt-4 bg-white border-t border-gray-200 pt-4 px-4">
+    <div className="relative">
+      <button
+        className="block px-4 py-2 text-gray-700 hover:bg-gray-100 w-full text-left"
+        onClick={() => setIsMobileDropdownOpen(!isMobileDropdownOpen)}
+      >
+        {language === "EN" ? "Recipes" : "Συνταγές"}
+      </button>
+      {isMobileDropdownOpen && (
+        <ul className="space-y-2 pl-4">
+          {categoryMapping[language]?.map((category) => (
+            <li key={category.path} className="p-2 hover:bg-gray-100">
+              <Link href={category.path} onClick={() => handleCategoryClick(category.path)}>
+                {category.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
           </div>
           <Link href="/about" className="block px-4 py-2 hover:bg-gray-100">
             {language === "EN" ? "About Grandpa" : "Σχετικά με τον Παππού"}
