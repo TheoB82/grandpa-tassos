5070763943ef504d1023dbe871f3a4b391eca214
TheoB82 <grandpatassos@gmail.com>
2025-04-01 07:06:31 +0100

    Updates2

--- src/app/page.js
+++ src/app/page.js
@@ -41,16 +41,14 @@ export default function Home() {
 
       {/* Embedded YouTube Video */}
       <div className="flex justify-center mb-10 min-h-[315px]">
-        <iframe 
-          width="560" 
-          height="315" 
-          src="https://www.youtube.com/embed/f_ZUEhudAWI?si=_-4Zyo3ZNiyjGJzt" 
-          title="YouTube video player" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          referrerPolicy="strict-origin-when-cross-origin" 
-          allowFullScreen>
-        </iframe>
+      <iframe width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/-pHu1H97DPA?si=IL5tYghx9QqW__Ct" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      referrerpolicy="strict-origin-when-cross-origin" 
+      allowfullscreen></iframe>
       </div>
 
       {/* Recipe Grid */}
