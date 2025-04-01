3dec352df62620c4d2d7490f0c6867783599d95f
TheoB82 <grandpatassos@gmail.com>
2025-04-01 22:30:33 +0100

    Updates2

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -131,21 +131,22 @@ const Header = () => {
       onChange={(e) => setSearchQuery(e.target.value)}
       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
     />
-    {searchResults.length > 0 && (
-      <ul className="mt-2 bg-white border border-gray-300 shadow-lg rounded-md z-50 max-h-64 overflow-y-auto">
-        {searchResults.map((recipe) => (
-          <li
-            key={recipe.TitleEN}
-            className="p-2 hover:bg-gray-100 cursor-pointer"
-            onClick={(event) => handleRecipeClick(recipe, event)}
-          >
-            <div className="font-semibold">{recipe[`Title${language}`]}</div>
-            <div className="text-sm text-gray-600">{recipe[`ShortDescription${language}`]}</div>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
+     {searchResults.length > 0 && (
+              <ul className="absolute left-0 mt-2 w-72 bg-white border border-gray-300 shadow-lg rounded-md z-50 max-h-64 overflow-y-auto">
+                {searchResults.map((recipe) => (
+                 <li
+                 key={recipe.TitleEN}
+                 className="p-2 hover:bg-gray-100 cursor-pointer"
+                 onClick={(event) => handleRecipeClick(recipe, event)}
+               >
+                 <div className="font-semibold">{recipe[`Title${language}`]}</div>
+                 <div className="text-sm text-gray-600">{recipe[`ShortDescription${language}`]}</div>
+               </li>
+               
+                ))}
+              </ul>
+            )}
+          </div>
 )}
 
       {/* Mobile Dropdown Menu */}
