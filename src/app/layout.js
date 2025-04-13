ae9f8d635549112071b3e539f6ea4e1758216aff
TheoB82 <grandpatassos@gmail.com>
2025-04-13 10:13:35 +0100

    asa

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,3 +23,32 @@ export default function Layout({ children }) {
     </LanguageProvider>
   );
 }
+import "../styles/globals.css";
+import { LanguageProvider } from "./context/LanguageContext"; // Correct path
+import Header from "./components/Header"; // Corrected path relative to src/app
+import CookieBanner from "./components/CookieBanner"; // Import CookieBanner component
+
+export const metadata = {
+  title: "Grandpa Tassos",
+  description: "Authentic Greek recipes passed down through generations.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Quicksand:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="antialiased">
+        <LanguageProvider>
+          <Header />
+          <div className="pt-52">{children}</div>
+          <CookieBanner /> {/* Add the CookieBanner here */}
+        </LanguageProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/metadata.js
+++
@@ -1,7 +0,0 @@
-// src/app/metadata.js
-
-export const metadata = {
-    title: "Grandpa Tassos",
-    description: "Authentic Greek recipes passed down through generations.",
-  };
-  
