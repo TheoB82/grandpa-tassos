2465ec9717118edc3017c89df75ad85b979503f8
TheoB82 <grandpatassos@gmail.com>
2025-04-13 19:41:02 +0100

    sd

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Quicksand:wght@400;700&family=Mynerve&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Mynerve&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Quicksand:wght@300..700&display=swap"
+    rel="stylesheet"
+  />
+</head>
+
       <body className="antialiased">
         <LanguageProvider>
           <Header />
