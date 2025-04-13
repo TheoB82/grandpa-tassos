eb29d156f0d0c0c003eb6638d99bff0dd28a3aa5
TheoB82 <grandpatassos@gmail.com>
2025-04-13 09:45:15 +0100

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
-import { LanguageProvider } from "./context/LanguageContext"; // Correct path
-import Header from "./components/Header"; // Corrected path relative to src/app
-import CookieBanner from "./components/CookieBanner"; // Import CookieBanner component
+import { LanguageProvider } from "./context/LanguageContext";
+import Header from "./components/Header";
+import CookieBanner from "./components/CookieBanner";
 
 export const metadata = {
   title: "Grandpa Tassos",
@@ -12,16 +12,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Quicksand:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2171074805444072"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className="antialiased">
         <LanguageProvider>
           <Header />
           <div className="pt-52">{children}</div>
-          <CookieBanner /> {/* Add the CookieBanner here */}
+          <CookieBanner />
         </LanguageProvider>
       </body>
     </html>
