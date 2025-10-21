961672750484dc779cc6d1862392dc6f704f5f50
TheoB82 <grandpatassos@gmail.com>
2025-10-21 21:33:01 +0100

    sldfkls

--- src/app/page.js
+++ src/app/page.js
@@ -44,7 +44,7 @@ export default function Home() {
 
       {/* Embedded YouTube Video */}
       <div className="flex justify-center mb-10 min-h-[315px]">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/D9yACdB-nr4?si=1yqmhuvHUERhzaKd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/4138sQVhC6A?si=Bz-vXYTyar4tTGay" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
        </div>
 
       {/* Recipe Grid */}
